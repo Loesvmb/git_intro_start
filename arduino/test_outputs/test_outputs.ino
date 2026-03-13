090504b6e90aec92e43206d777ff412a092fa759
Loes Vanmeerbeek <loes.vanmeerbeek@UGent.be>
2026-03-13 17:33:14 +0100

    toevoeging: Setup(): Serial.begin(115200); Loop(): Serial.println(“hello”);

--- arduino/test_outputs/test_outputs.ino
+++ arduino/test_outputs/test_outputs.ino
@@ -1,10 +1,12 @@
 int ledPin = 3;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  Serial.println(“hello”);
   digitalWrite(ledPin, HIGH);  
   delay(1000);                      
   digitalWrite(ledPin, LOW);   
